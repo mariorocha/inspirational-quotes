703d6537bcab43ebed62845c484f7edcb75d6553
Mário Rocha <mario.rocha@infraspeak.com>
2023-02-06 12:00:16 +0000

    change quote generation

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 ### Node.js project for dev ops with docker course
 An exercise from the course required me to create a docker image of a public repository and publish it to docker hub
 
-So I wrote this is a simple node project that outputs a random message each 5 seconds.
+So I wrote this is a simple node project that outputs a random message.
 
 # Setup project
 1) Install node.js on your machine (an LTS version for example)
 2) And then execute the command ``node main.js``
-3) This script should output a random message each 5 seconds.
+3) This script should output a random message.
--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const quotes = [  "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. – Christian D. Larson",  "The future belongs to those who believe in the beauty of their dreams. – Eleanor Roosevelt",  "Your only limit is the amount of love you give. – LeCrae",  "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",  "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",  "God gives his greatest challenges to his greatest warriors"];
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function getQuote() {
     const quote = quotes[Math.floor(Math.random() * quotes.length)];
     console.log(quote);
+
+    setTimeout(function() {
+        rl.question("Do you want a new quote? (yes/no) ", function(answer) {
+            if (answer === "yes") {
+                getQuote();
+            } else {
+                console.log("Goodbye!");
+                rl.close();
+            }
+        });
+    }, 1000);
 }
 
 getQuote();
-setInterval(getQuote, 5000);

9d756d90de5dab287b6145b10de05828cf9423cb
Mário Rocha <mario.rocha@infraspeak.com>
2023-02-06 10:51:36 +0000

    add node sample to print a inspirational quote each 5 secounds

---
+++ main.js
@@ -0,0 +1,8 @@
+const quotes = [  "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. – Christian D. Larson",  "The future belongs to those who believe in the beauty of their dreams. – Eleanor Roosevelt",  "Your only limit is the amount of love you give. – LeCrae",  "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",  "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",  "God gives his greatest challenges to his greatest warriors"];
+
+function getQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    console.log(quote);
+}
+
+setInterval(getQuote, 5000);
